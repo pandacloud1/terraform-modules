26592ce2d12f616436023a8c6d6738fa87ca0346
PandaCloud <134182273+pandacloud1@users.noreply.github.com>
2025-09-02 17:18:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,21 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
 module "vpc" {
   source       = "./modules/vpc"
+  cidr_block   = var.cidr_block
+  subnet_cidr  = var.subnet_cidr
+  az           = var.az
+  vpc_name     = var.vpc_name
 }
 
 module "ec2" {
   source         = "./modules/ec2"
+  ami            = var.ami
+  instance_type  = var.instance_type
+  subnet_id      = module.vpc.subnet_id
+  key_name       = var.key_name
+  instance_name  = var.instance_name
 }
 
 module "s3" {
   source      = "./modules/s3"
+  bucket_name = var.bucket_name
 }
