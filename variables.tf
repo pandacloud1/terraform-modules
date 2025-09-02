4203351d59b385fde5200ad4c3bad3a4546b7a29
PandaCloud <134182273+pandacloud1@users.noreply.github.com>
2025-09-02 17:18:13 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "cidr_block" {}
+variable "subnet_cidr" {}
+variable "az" {}
+variable "vpc_name" {}
+
+variable "ami" {}
+variable "instance_type" {}
+variable "key_name" {}
+variable "instance_name" {}
+
+variable "bucket_name" {}
