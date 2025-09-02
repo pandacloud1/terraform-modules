c18bf12d0b405a9dd3cc8f7dc2937ed1dc0a7ce3
PandaCloud <134182273+pandacloud1@users.noreply.github.com>
2025-09-02 17:38:52 +0530

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,23 @@
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = module.vpc.vpc_id
+}
+
+output "subnet_id" {
+  description = "The ID of the subnet"
+  value       = module.vpc.subnet_id
+}
+
+output "ec2_instance_id" {
+  description = "The ID of the EC2 instance"
+  value       = module.ec2.instance_id
+}
+
+output "ec2_public_ip" {
+  description = "The public IP of the EC2 instance"
+  value       = module.ec2.public_ip
+}
+
+output "bucket_id" {
+  value = module.s3.bucket_id
+}
